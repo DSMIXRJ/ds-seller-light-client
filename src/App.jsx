c32cc63eed6900e4bc5f5cb749872d94ecf5a698
DSMIXRJ <dsmixrj@gmail.com>
2025-04-29 19:20:36 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const baseUrl = import.meta.env.VITE_PUBLIC_URL || window.location.origin;
+const baseUrl = 'https://ds-seller-light-backend.onrender.com';
 
 function App() {
   const [count, setCount] = useState(0)
