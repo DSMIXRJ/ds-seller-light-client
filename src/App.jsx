3ccb44ed380b09ad409314d2a0a0aa3096c324e7
DSMIXRJ <dsmixrj@gmail.com>
2025-05-23 19:41:24 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Dashboard from './Dashboard';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
+}
+
+export default App;
 import Dashboard from './Dashboard';
 
 function App() {
