d27afad99845b02640db74402296be716a611e71
DSMIXRJ <dsmixrj@gmail.com>
2025-05-30 18:49:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Anuncios from "./pages/Anuncios";
-// import Integracoes from "./Integracoes"; // Removido pois não existe
+import Login from "./Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/anuncios/:integracao" element={<Anuncios />} />
-        {/* Adicione aqui outras rotas reais do seu projeto, como Login, etc. */}
       </Routes>
     </BrowserRouter>
   );
