300ba3d80d5bd67d8f3340cdc130a74e581b4231
DSMIXRJ <dsmixrj@gmail.com>
2025-06-12 18:45:38 -0300

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,14 +3,12 @@ import Topbar from "../components/Topbar";
 
 export default function MainLayout({ children, activePage }) {
   return (
-    <div className="flex min-h-screen bg-gradient-to-br from-zinc-950 via-zinc-900 to-zinc-800 text-white">
+    <div className="flex min-h-screen bg-gradient-to-br from-zinc-950 via-zinc-900 to-zinc-800 text-zinc-50">
       <Sidebar activePage={activePage} />
-      <div className="flex-1 flex flex-col ml-56">
+      <main className="flex-1 flex flex-col items-center ml-56">
         <Topbar />
-        <main className="flex-1 p-6 mt-20">
-          {children}
-        </main>
-      </div>
+        <div className="w-full max-w-6xl pt-[120px]">{children}</div>
+      </main>
     </div>
   );
 }
