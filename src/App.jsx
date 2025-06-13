2ae45b971017b7aa8772c4705888115821062ca4
DSMIXRJ <dsmixrj@gmail.com>
2025-06-13 16:05:14 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,19 @@ import EscolherMarketplace from "./pages/EscolherMarketplace";
 import Integracoes from "./pages/Integracoes";
 import ProductTable from "./components/ProductTable";
 import AuthCallback from "./pages/AuthCallback";
-import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/escolher-marketplace" element={<EscolherMarketplace />} />
-          <Route path="/integracoes" element={<Integracoes />} />
-          <Route path="/produtos" element={<ProductTable />} />
-          <Route path="/auth/callback" element={<AuthCallback />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/escolher-marketplace" element={<EscolherMarketplace />} />
+        <Route path="/integracoes" element={<Integracoes />} />
+        <Route path="/produtos" element={<ProductTable />} />
+        <Route path="/auth/callback" element={<AuthCallback />} />
+      </Routes>
+    </Router>
   );
 }
 
