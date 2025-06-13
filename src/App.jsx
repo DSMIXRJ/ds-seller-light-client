38a16586d45efd76a40369c2ddc9f77a102f5d5c
DSMIXRJ <dsmixrj@gmail.com>
2025-06-13 15:42:31 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Login from "./Login";
 import Dashboard from "./Dashboard";
 import EscolherMarketplace from "./pages/EscolherMarketplace";
 import Integracoes from "./pages/Integracoes";
-import ProductTable from "./pages/ProductTable";
+import ProductTable from "./components/ProductTable";
 import AuthCallback from "./pages/AuthCallback";
 import { AuthProvider } from "./context/AuthContext";
 
