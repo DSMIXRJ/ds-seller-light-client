0963a8663b706bcbebc9300956aec6ce65b145c3
DSMIXRJ <dsmixrj@gmail.com>
2025-06-24 17:26:37 -0300

    Update TableHeader.jsx

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -13,30 +13,20 @@ const TableHeader = ({ table }) => {
               className="px-1 py-3 text-center bg-[#181c2f] border-b-2 border-cyan-500/50"
               style={{ width: header.column.columnDef.size }}
             >
-              {header.isPlaceholder ? null : (
-                <div
-                  {...{
-                    className:
-                      'flex items-center justify-center whitespace-nowrap overflow-hidden text-ellipsis underline',
-                    onClick: header.column.getCanSort()
-                      ? header.column.getToggleSortingHandler()
-                      : undefined,
-                  }}
-                >
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext()
-                  )}
-                  {header.column.getCanSort() && (
-                    <span className="text-gray-400 ml-1">
-                      {{
-                        asc: '▲',
-                        desc: '▼',
-                      }[header.column.getIsSorted()] ?? '▲▼'}
-                    </span>
-                  )}
-                </div>
-              )}
+              <div
+                className="flex items-center justify-center whitespace-nowrap overflow-hidden text-ellipsis underline"
+                onClick={header.column.getCanSort() ? header.column.getToggleSortingHandler() : undefined}
+              >
+                {flexRender(header.column.columnDef.header, header.getContext())}
+                {header.column.getCanSort() && (
+                  <span className="text-gray-400 ml-1">
+                    {{
+                      asc: '▲',
+                      desc: '▼',
+                    }[header.column.getIsSorted()] ?? '▲▼'}
+                  </span>
+                )}
+              </div>
             </th>
           ))}
         </tr>
